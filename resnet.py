3b3b3e6935880aebf9d31295c99f915a7b5338dd
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2022-11-25 13:20:11 +0100

    val dataset transformed to tensor

--- resnet.py
+++ resnet.py
@@ -7,66 +7,8 @@ import torchvision.transforms as transforms
 from torchvision.models import resnet50, ResNet50_Weights
 from torchmetrics import Recall, Precision, F1Score
 import copy
-
-
-def train(model, device, train_loader, criterion, optimizer, metrics, epoch):
-    model.train()
-    sum_loss = 0
-    n_samples = 0
-    n_batches = 0
-    correct = 0
-
-    recall_sum = 0
-    precision_sum = 0
-    f1_sum = 0
-    for batch_id, (data, labels) in enumerate(train_loader):
-        n_batches += 1
-        n_samples += len(data)
-
-        data, labels = data.to(device), labels.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        pred = output.argmax(dim=1, keepdim=True)
-
-        correct += pred.eq(labels.view_as(pred)).sum().item()
-
-        loss = criterion(output, labels)
-        loss.backward()
-        optimizer.step()
-
-
-        sum_loss += loss.item()
-        recall_sum += metrics["recall"](output, labels).item()
-        precision_sum += metrics["precision"](output, labels).item()
-        f1_sum += metrics["f1score"](output, labels).item()
-
-    avg_loss = sum_loss / n_samples
-    accuracy = 100 * correct / n_samples
-    avg_recall = recall_sum / n_batches
-    avg_precision = precision_sum / n_batches
-    avg_f1 = f1_sum / n_batches
-
-    print(f"Epoch={epoch}, avg_loss={avg_loss:.3f}, acc={accuracy:.2f}, recall={avg_recall:.2f}, precision={avg_precision:.2f}, f1={avg_f1:.2f}")
-    return avg_loss, accuracy, avg_recall, avg_precision, avg_f1
-
-
-def validate(model, device, test_loader, criterion):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, label in test_loader:
-            data, label = data.to(device), label.to(device)
-            output = model(data)
-            test_loss += criterion(output, label).item()
-            pred = output.argmax(dim=1, keepdim=True)
-            correct += pred.eq(label.view_as(pred)).sum().item()
-
-    test_loss /= len(test_loader.dataset)
-    accuracy = 100 * correct / len(test_loader.dataset)
-
-    print(f"\tVAL: Loss={test_loss:.3f}, Acc={accuracy:.1f}")
-    return test_loss, accuracy
+from train import train
+from validate import validate
 
 
 def main():
@@ -90,7 +32,7 @@ def main():
         ]
     )
     train_dataset = ImageFolder(root=TRAIN_PATH, transform=t)
-    test_dataset = ImageFolder(root=TEST_PATH)
+    test_dataset = ImageFolder(root=TEST_PATH, transform=transforms.ToTensor())
     train_loader = DataLoader(train_dataset, shuffle=True, batch_size=BATCH_SIZE)
     test_loader = DataLoader(test_dataset)
 
@@ -121,16 +63,16 @@ def main():
     best_model_wts = copy.deepcopy(model.state_dict())
 
     for epoch in range(1):
-        loss, acc, recall, prec, f1 = train(model, device, train_loader, criterion, optimizer, metrics, epoch)
-        train_acc.append(acc)
-        train_loss.append(loss)
-        train_recall.append(recall)
-        train_precision.append(prec)
-        train_f1score.append(f1)
+        # loss, acc, recall, prec, f1 = train(model, device, train_loader, criterion, optimizer, metrics, epoch)
+        # train_acc.append(acc)
+        # train_loss.append(loss)
+        # train_recall.append(recall)
+        # train_precision.append(prec)
+        # train_f1score.append(f1)
 
-        # v_loss, v_acc = validate(model, device, test_dataset, criterion)
-        # val_acc.append(v_acc)
-        # val_loss.append(v_loss)
+        v_loss, v_acc = validate(model, device, test_loader, criterion)
+        val_acc.append(v_acc)
+        val_loss.append(v_loss)
 
         # if v_acc > best_val_acc:
         #     best_val_acc = v_acc
@@ -142,7 +84,6 @@ def main():
     print(train_recall)
     print(train_precision)
     print(train_f1score)
-    # draw_plots() TODO
 
 
 if __name__ == "__main__":
